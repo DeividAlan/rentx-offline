cdb97a7228a5fc25391d2fe16141a3480f59ba3b
Deivid Alan <deividalan@hotmail.com>
2021-10-12 16:55:40 -0300

    updated

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -43,7 +43,7 @@ export function Home(){
     return {
       transform: [
         { translateX: positionX.value },
-        { translateY: positionY.value }
+        { translateY: positionY.value },
       ]
     }
   });
